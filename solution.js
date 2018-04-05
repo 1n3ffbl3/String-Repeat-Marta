d85b0bd6aa87fe603f4595536608f6d92cd4e869
1n3ffbl3 <37382210+1n3ffbl3@users.noreply.github.com>
2018-04-05 14:50:30 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,11 @@
 
+ function solution(string) {
+    let reversed_string = "";
+    let temp_array = [];
+    
+    temp_array = string.split("");
+    temp_array = temp_array.reverse();
+    reversed_string = temp_array.join("");
+    
+    return reversed_string;
+}; 
