dc1348a8fa058aa3380a94214fbd1c86c2dfba74
1n3ffbl3 <37382210+1n3ffbl3@users.noreply.github.com>
2018-04-05 23:24:41 +0200

    Refactoring function name

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
-function repeat_String_NumTimes(n, string) {
+function repeatStringNTimes(n, string) {
   if (n > 0)
     return string.repeat(n);
   else
     return "";
 }
-repeat_String_NumTimes(2, "pitu");
+repeatStringNTimes(2, "pitu");
