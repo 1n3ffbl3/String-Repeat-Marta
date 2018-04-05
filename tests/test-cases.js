5466ffa5ba11e78854db5cb05211b0734d4e7440
1n3ffbl3 <37382210+1n3ffbl3@users.noreply.github.com>
2018-04-05 14:54:58 +0200

    Update test-cases.js

--- tests/test-cases.js
+++ tests/test-cases.js
@@ -1,1 +1,20 @@
 
+tests({
+
+  'eert': function() {
+    eq("tree", solution("eert"));
+  },
+  'car': function() {
+    eq("rac", solution("car"));
+  },
+  'pencil': function() {
+    eq("licnep", solution("pencil"));
+  },
+  '8873024': function() {
+    eq("4203788", solution("8873024"));
+  },
+  '##&][&##': function() {
+    eq("##&][&##", solution("##&][&##"));
+  }
+
+});
