bb79103e58e94f67e454c3a53db52e9fbc49b7f8
1n3ffbl3 <37382210+1n3ffbl3@users.noreply.github.com>
2018-04-06 00:35:14 +0200

    Adding additional solution with exception

--- solution.js
+++ solution.js
@@ -1,7 +1,18 @@
 function repeatStringNTimes(n, string) {
-  if (n > 0)
-    return string.repeat(n);
-  else
+  if (n <= 0) {
     return "";
+  }
+  return string.repeat(n);
 }
 repeatStringNTimes(2, "pitu");
+
+
+/*
+Second solution
+function repeatStringNTimes(n, string) {
+  if (n <= 0) {
+    throw "n must be positive number";
+  }
+  return string.repeat(n);
+}
+*/
